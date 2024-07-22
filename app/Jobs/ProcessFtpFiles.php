e9ad797e1927634767a21ec84ebbc6ed7767beba
frank <orestes.apodaca@gmail.com>
2024-07-22 12:41:12 -0700

    corregir el procesado del archivo y mayus minus extension ftp

--- app/Jobs/ProcessFtpFiles.php
+++ app/Jobs/ProcessFtpFiles.php
@@ -151,25 +151,21 @@ class ProcessFtpFiles implements ShouldQueue
         return false;
     }
     
+    
+
     private function processFile($filePath)
     {
-        Log::info('Entrando a Procesado de Archivo', ['filePath' => $filePath, 'memory_usage' => memory_get_usage()]);
+       // Log::info('Entrando a Procesado de FTP', ['filePath' => $filePath, 'memory_usage' => memory_get_usage()]);
     
         try {
             $errorFound = false;
             $errorMessage = '';
             $fileContents = [];
     
-            // Verificar la existencia del archivo antes de abrirlo
-            if (!file_exists($filePath)) {
-                Log::error('Archivo no encontrado antes de fopen', ['filePath' => $filePath]);
-                return;
-            }
-    
             // Leer el archivo completamente
             $file = fopen($filePath, 'r');
             if ($file) {
-                Log::info('Inicia Procesado de Archivo', ['filePath' => $filePath, 'memory_usage' => memory_get_usage()]);
+            //    Log::info('Inicia Procesado de Archivo FTP', ['filePath' => $filePath, 'memory_usage' => memory_get_usage()]);
     
                 while (($line = fgets($file)) !== false) {
                     $fileContents[] = $line;
@@ -179,12 +175,189 @@ class ProcessFtpFiles implements ShouldQueue
                 Log::error('No se pudo abrir el archivo', ['filePath' => $filePath]);
                 return;
             }
-            // Continuar con el procesamiento del archivo
+    
+    
+            foreach ($fileContents as $line) {
+                $data = explode('|', $line);
+    
+                if ($data[0] == '501') {
+                    
+                    // Buscar el ID de la empresa usando la clave
+                    $empresa = Empresa::where('clave', $data[4])->first();
+
+    //SINO EXISTE LA EMPRESA
+
+                    if (!$empresa) {
+                        $errorMessage = '*1003|Empresa no encontrada para la clave: ' . $data[4] . PHP_EOL;
+                    
+                        // Verificar si ya existen mensajes 1003 en el archivo
+                        $existingMessages = array_filter($fileContents, function($line) {
+                            return strpos($line, '*1003|') === 0;
+                        });
+                    
+                        if (empty($existingMessages)) {
+                            // Verificar si el mensaje ya existe en el archivo original
+                            if (!in_array(trim($errorMessage), array_map('trim', $fileContents))) {
+                                $file = fopen($filePath, 'a');
+                                if ($file) {
+                                    fwrite($file, $errorMessage);
+                                    fclose($file);
+                                }
+                          //      Log::info('Mensaje de error añadido al archivo original', ['filePath' => $filePath, 'errorMessage' => $errorMessage]);
+                            }
+                        } else {
+                         //   Log::info('Mensajes de estado ya existentes, no se agregarán nuevos mensajes', ['filePath' => $filePath]);
+                        }
+                    
+                        $errorFound = true;
+                        break; // Romper el bucle y pasar al siguiente archivo
+                    }
+                    
+//CREAR VALIDACION
+
+                    $validatedData = [
+                        'factura' => 'required|string|max:255',
+                        'empresa_id' => 'required|exists:empresas,id',
+                        'fechaReg' => 'required|date',
+                        'adjuntoFactura' => 'required|string',
+                        'aduana' => 'required|string',
+                        'patente' => 'required|string',
+                        'Toperacion' => 'required|string',
+                    ];
+    
+                    $validator = Validator::make([
+                        'factura' => $data[16],
+                        'empresa_id' => $empresa->id,
+                        'fechaReg' => Carbon::now(), // Esto debería ser la fecha real del archivo o la fecha de recepción
+                        'adjuntoFactura' => '/storage/invoices/' . basename($filePath),
+                        'aduana' => '400-TIJ', // Cambiar según el contexto real
+                        'patente' => '3875', // Cambiar según el contexto real
+                        'Toperacion' => $data[1] == '1' ? 'Importacion' : 'Exportacion', // Determinar la operación basada en el valor
+                    ], $validatedData);
+    
+
+// SI VALIDACION FALLA   
+
+                    if ($validator->fails()) {
+                        $errorMessage = '*1001|Validación fallida para la factura: ' . $data[16] . PHP_EOL;
+                    
+                        // Verificar si ya existen mensajes 1000, 1001, o 1003
+                        $existingMessages = array_filter($fileContents, function($line) {
+                            return  strpos($line, '*1001|');
+                        });
+                    
+                        if (empty($existingMessages)) {
+                            // Verificar si el mensaje ya existe en el archivo original
+                            if (!in_array(trim($errorMessage), array_map('trim', $fileContents))) {
+                                $file = fopen($filePath, 'a');
+                                if ($file) {
+                                    fwrite($file, $errorMessage);
+                                    fclose($file);
+                                }
+                          //      Log::info('Mensaje de error añadido al archivo original', ['filePath' => $filePath, 'errorMessage' => $errorMessage]);
+                            }
+                        } else {
+                          //  Log::info('Mensajes de estado ya existentes, no se agregarán nuevos mensajes', ['filePath' => $filePath]);
+                        }
+                    
+                        $errorFound = true;
+                        break; // Romper el bucle y pasar al siguiente archivo si la validación falla
+                    }
+                    
+    // CREAR TRAFICO
+
+                    $trafico = Trafico::create($validator->validated());
+                    $trafico->MxDocs = "PENDIENTE";
+                    $trafico->statusTrafico = "ABIERTO";
+    
+                    // Mover el archivo TXT y PDF a la nueva ubicación
+                    $nombreOriginalTxt = basename($filePath);
+                    $nombreOriginalPdf = str_ireplace('.txt', '.pdf', $nombreOriginalTxt); // str_ireplace es insensible a mayúsculas y minúsculas
+    
+                    // Mover el archivo TXT
+                    $rutaArchivoTxt = Storage::disk('local')->move(
+                        'invoices/' . $nombreOriginalTxt,
+                        'public/Facturas/FacturaTrafico_' . $trafico->id . '/' . $nombreOriginalTxt
+                    );
+    
+                  //  Log::info('Archivo TXT movido', ['filename' => $nombreOriginalTxt, 'new_location' => $rutaArchivoTxt]);
+    
+                    // Mover el archivo PDF
+                    if (Storage::disk('local')->exists('invoices/' . $nombreOriginalPdf)) {
+                        $rutaArchivoPdf = Storage::disk('local')->move(
+                            'invoices/' . $nombreOriginalPdf,
+                            'public/Facturas/FacturaTrafico_' . $trafico->id . '/' . $nombreOriginalPdf
+                        );
+    
+                      //  Log::info('Archivo PDF movido', ['filename' => $nombreOriginalPdf, 'new_location' => $rutaArchivoPdf]);
+                    } else {
+                        $rutaArchivoPdf = null;
+                        Log::warning('Archivo PDF no encontrado', ['filename' => $nombreOriginalPdf]);
+                    }
+    
+                    $trafico->adjuntoFactura = '/Facturas/FacturaTrafico_' . $trafico->id . '/' . $nombreOriginalPdf;
+    
+    //REVISION CREAR EN TRUE
+
+                    if (true) { // Supongamos que siempre lleva revisión
+                        $revision = new Revisione();
+                        $revision->nombreRevisor = 'sinAsignar';
+                        $revision->facturaCorrecta = $trafico->fechaReg;
+                        $revision->status = 'PENDIENTE';
+                        $revision->ubicacionRevision = 'DefaultLocation'; // Cambiar según el contexto real
+                        $revision->correccionFactura = 'NO';
+                        $revision->save();
+    
+                        $trafico->revision()->associate($revision);
+                        $trafico->Revision = 'PENDIENTE';
+                        $trafico->revision_id = $revision->id;
+                    } else {
+                        $trafico->Revision = 'N/A';
+                    }
+    
+                    $trafico->save();
+    
+    // Emitir el evento y registrar en el historial
+
+                    event(new TraficoCreated($trafico));
+    
+    //CREA EL HISTORIAL
+
+                    Historial::create([
+                        'trafico_id' => $trafico->id,
+                        'nombre' => 'Recepción de Factura',
+                        'descripcion' => 'Recepción de Factura se inicia nuevo Proceso de Tráfico.',
+                        'hora' => Carbon::now('America/Los_Angeles'),
+                        'adjunto' => $trafico->adjuntoFactura,
+                    ]);
+    
+    //CREA EL METODO DE ENVIAR CORREO
+
+                    // Enviar la factura por correo
+                    $this->sendFacturaByEmail($trafico);
+    
+                    // Escribir el mensaje de error o confirmación en el archivo en su nueva ubicación
+                    $newFilePath = storage_path('app/public/Facturas/FacturaTrafico_' . $trafico->id . '/' . $nombreOriginalTxt);
+                    if (!$errorFound) {
+                        $errorMessage = '*1000|Factura Ingresada con éxito' . PHP_EOL;
+                    }
+
+                    $newFile = fopen($newFilePath, 'a');
+
+                    if ($newFile) {
+                        fwrite($newFile, $errorMessage);
+                        fclose($newFile);
+                    }
+    
+                  //  Log::info('Archivo procesado y actualizado', ['filePath' => $newFilePath, 'errorFound' => $errorFound]);
+                }
+            }
         } catch (\Exception $e) {
             Log::error('Error al procesar el archivo', ['error' => $e->getMessage(), 'filePath' => $filePath]);
         }
     }
     
+
 
     private function sendFacturaByEmail($trafico)
     {
